580574c193d77b58e1e6a252ca4670dfd98389f2
KProsser1 <126711264+KProsser1@users.noreply.github.com>
2023-05-09 14:36:43 -0400

    Added auto scroll to banner, needs tweaking

--- home.js
+++ home.js
@@ -1,11 +1,79 @@
-let slideIndex = 1;
+let slides = document.querySelectorAll(".slideLayout"),
+arrowLeft = document.querySelector(".prev"),
+arrowRight = document.querySelector(".next"),
+slideIndex = 0;
+
+function reset() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+}
+
+function startSlide() {
+    reset();
+    slides[0].style.display = "block";
+}
+
+function slideLeft() {
+    reset();
+    slides[slideIndex - 1].style.display = "block";
+    slideIndex --;
+}
+
+function slideRight() {
+    reset();
+    slides[slideIndex + 1].style.display = "block";
+    slideIndex ++;
+}
+
+arrowLeft.addEventListener("click", function () {
+    if (slideIndex === 0) {
+        slideIndex = slides.length;
+    }
+    slideLeft();
+});
+
+arrowRight.addEventListener("click", function () {
+    if (slideIndex === slides.length - 1) {
+        slideIndex = -1;
+    }
+    slideRight();
+})
+
+startSlide();
+
+//automatic scroll
+
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slideLayout");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 7000);
+    console.log(slideIndex);
+}
+
+
+//clickable scroll
+/*let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function slideIndexSlide(n) {
     showSlides(slideIndex = n);
 }
 
