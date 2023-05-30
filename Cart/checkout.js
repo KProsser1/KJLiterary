443ecd5d8dafd335c90ab6d90807b8c786da7df8
KProsser1 <126711264+KProsser1@users.noreply.github.com>
2023-05-30 15:01:35 -0400

    git

---
+++ Cart/checkout.js
@@ -0,0 +1,16 @@
+/*Menu change for mobile*/
+function goToNewPage() {
+    var url = document.getElementById('list').value;
+    if(url != 'none') {
+        window.location = url;
+    }
+}
+
+
+function validateForm() {
+    let x = document.forms["cartForm"]["fname"].value;
+    if (x == "") {
+        alert(fname + "must be filled out")
+        return false;
+    }
+}
