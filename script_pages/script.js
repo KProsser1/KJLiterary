98ab3156edec19a7fa0fefea04fdebdca70cec66
KProsser1 <126711264+KProsser1@users.noreply.github.com>
2023-07-12 15:09:50 -0400

    Corrected company name, added content

--- script_pages/script.js
+++ script_pages/script.js
@@ -1,3 +1,17 @@
+draculaPreview.addEventListener("click", function() {
+    draculaPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function(e) {
+    e.preventDefault();
+    draculaPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == draculaPopup) {
+        draculaPopup.classList.remove("show");
+    }
+});
+
+
 function goToNewPage() {
     var url = document.getElementById('list').value;
     if(url != 'none') {
